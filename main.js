ec3fea92ac38b2efb3db3db17ee25e309af92f8a
yossefelbana7 <yosefelbana7@gmail.com>
2024-03-12 21:03:36 +0200

    first tasks

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let continer = document.querySelector('.continer');
+
+
+function phones(phon){
+    img.src = phon
+}
+function colors(color){
+    continer.style.background = color
+}
